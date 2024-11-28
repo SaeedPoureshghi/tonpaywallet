5bf4e048aacd814c733d3b7b12dc791a7f7a1267
SaeedPoureshghi <saeed.po@gmail.com>
2024-11-28 10:03:07 +0300

    Integrate @twa-dev/sdk for safe area insets in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Flex } from "antd"
 import "./App.css"
 import Home from "./pages/Home"
+import WebApp from "@twa-dev/sdk"
 const App = () => {
+
+  WebApp.ready();
+
   return (
-    <Flex vertical>
+    <Flex vertical
+    style={{
+      paddingTop: WebApp?.contentSafeAreaInset.top,
+      paddingBottom: WebApp?.contentSafeAreaInset.bottom,
+      paddingLeft: WebApp?.contentSafeAreaInset.left,
+      paddingRight: WebApp?.contentSafeAreaInset.right,
+    }}
+    >
       <Home/>
 
     </Flex>
